ed1a01085393e5748c94776ec2e082d35f807d47
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-07-27 12:31:30 +0530

    signup and signin  Api constructed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,22 +1,127 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const USERS = require("../models/user");
+const bcrypt = require('bcrypt');
+const { jwt_secret } = require("../keys")
+const jwt = require("jsonwebtoken")
+const requireLogin = require("../middlewares/requireLogin")
 
 
+// Signup Route
+router.get("/signup", (req, res) => {
+    res.json({ message: "Signup page GET request" });
+});
+
+router.post("/signup", (req, res) => {
+    const { firstName,
+        lastName,
+        userName,
+        dob,
+        gender,
+        nationality,
+        emailAddress,
+        mobileNumber,
+        emailVerified,
+        mobileVerified,
+        govIdInfo,
+        city,
+        wardNo,
+        zoneNo,
+        homeAddress,
+        occupation,
+        preferredLanguage,
+        alternateNumber,
+        tncAccepted,
+        password
+    } = req.body;
 
 
+    // Check for required fields
+    if (!firstName || !lastName || !userName||  !dob || !gender || !emailAddress ||
+        !mobileNumber || !govIdInfo || !city || !wardNo || !zoneNo ||
+        !homeAddress || !tncAccepted || !password) {
+        console.log("Enter all the required fields");
+        return res.status(400).send("Please fill all the required fields");
+    }
 
+    // Check if user with the same government ID is already registered
+    USERS.findOne({ govIdInfo: govIdInfo })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.json({ message: "User with this government ID is already registered" });
+            }
 
+            // Hash the password and save the user
+            bcrypt.hash(password, 10).then((hashedPassword) => {
+                const user = new USERS({
+                    firstName,
+                    lastName,
+                    userName,
+                    dob,
+                    gender,
+                    nationality,
+                    emailAddress,
+                    mobileNumber,
+                    emailVerified,
+                    mobileVerified,
+                    govIdInfo,
+                    city,
+                    wardNo,
+                    zoneNo,
+                    homeAddress,
+                    occupation,
+                    preferredLanguage,
+                    alternateNumber,
+                    tncAccepted,
+                    password: hashedPassword,
+                });
 
-// Signup  Route ----------- ------------ ----------- ------------ ------------ ----------
+                user.save()
+                    .then((savedUser) => {
+                        console.log(savedUser);
+                        return res.json({ message: "Signup successful" });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        return res.status(500).send("Error saving the user");
+                    });
+            }).catch(err => {
+                console.log(err);
+                return res.status(500).send("Error hashing the password");
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).send("Error checking existing user");
+        });
+});
 
-router.get("/signup",(req,res)=>{
-    res.json({message:"signup page get request"})
+router.post("/signin", (req, res) => {
+    const { govIdInfo , mobileNumber ,  password , userName  } = req.body;
+
+    if (!govIdInfo) {
+        return res.status(422).json({ error: "Please Add Email and Password" })
+    }
+
+    USERS.findOne({ userName: userName })
+        .then((savedUser) => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "Invalid username" })
+            }
+            bcrypt.compare(password, savedUser.password)
+                .then((match) => {
+                    if (match) {
+                        const token = jwt.sign({ _id: savedUser.id }, jwt_secret)
+                        // console.log(token)
+                        return res.status(422).json({ message: "Signin Successful", token: token })
+                    }
+                    else {
+                        return res.status(422).json({ error: "Invalid Password" })
+                    }
+                })
+                .catch(err => console.log(err))
+        })
+
 })
-router.post("/signup",()=>{
-    const {firstname , lastname , dob , gender , nationality , email , mobileNumber , emailVerified , mobileVerified , govtIdInfo , residentialAddress ,idProof ,occupation , prefferedLanguage , alternateNumber , tncAccept} = req.body;
-    res.send("Post api is succesful")
-})
 
-
- module.exports=router;
-
+module.exports = router;
