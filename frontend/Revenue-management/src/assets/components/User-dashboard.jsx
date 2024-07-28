90988f29f5c3daa95f3962b48c710ab22c0a1054
HARSHILd11 <dhoot.harshil@gmail.com>
2024-07-28 11:05:11 +0530

    few changes

--- frontend/Revenue-management/src/assets/components/User-dashboard.jsx
+++ frontend/Revenue-management/src/assets/components/User-dashboard.jsx
@@ -3,33 +3,34 @@ import Chart from './Chart';
 import Piechart from "./Piechart1";
 import "./User-dashboard.css"
 
-
+import BarChart from "./WaterTaxGraph"
+import PropertyChart from "./PropertyTaxGraph"
 
 export default function DashBoard() {
   return (
     <>
 
       <div className="Dashboard-container1">
-        <div className="upper-dashboard">
-          <div className="status-display-container">
-            <div className="status-display">
+        <div className="upper-dashboard " id='bdr'>
+          <div className="status-display-container" id='bdr'>
+            <div className="status-display" id='bdr'>
               <div className="status-box sb-1">Balance: <b>-10000</b> </div>
               <div className="status-box sb-2">Payment Status : <br /> <b> pending</b> </div>
               <div className="status-box sb-3">Your Assets</div>
             </div>
-            <div className="line-graph">
+            <div className="line-graph" id='bdr'>
               <Chart />
             </div>
           </div>
-          <div className="payment-display">
+          <div className="payment-display" id='bdr'>
             <div className='tax-paid'>
               <div>
-                Total Tax Paid till now
+                Total Tax Paid till now <div className='piechart'><Piechart></Piechart></div>
               </div>
               <div>
                 <b>100203</b>
               </div> </div>
-          <div className="tax-paid">
+          <div className="tax-paid" id='bdr'>
           <div>
               Dues : 4000
             </div>
@@ -37,19 +38,25 @@ export default function DashBoard() {
             <button className='btn btn-danger paynowsty'>Paynow</button>
             </div>
           </div>
-          <div className='tax-paid'>
+          <div className='tax-paid' id='bdr'>
           <div>Pts Earned 100 </div>
           <div><button>Reedem NOW</button></div>
           </div>
 
           </div>
         </div>
-        <div className="lower-dashboard">
-          <div className="piechart">
-            <Piechart></Piechart>
+        <div className="lower-dashboard" id='bdr'>
+        <div className='Water-Tax' id='bdr'>
+            <span>Water Tax</span>
+            <BarChart></BarChart>
           </div>
           <div className='ld-status-box status-box'>Payment history
 
+          </div>
+          
+          <div className='Property-Tax' id='bdr'>
+            <span>Property Tax</span>
+            <PropertyChart></PropertyChart>
           </div>
          
         </div>
